faaf183968e10654e48bde0dd10eb7bdd32e2fa9
lverghese <loganverghese@gmail.com>
2021-07-27 22:45:21 -0400

    button listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,38 @@
 const APIKey = "40360ebbfafb9db4e0cd8e687316fac5";
 
 
-//all variables to match html ids
+//clearing local storage on button click
+var clearBtn = document.getElementById("clearHistoryBtn");
+clearBtn.addEventListener("click", function() {
+    localStorage.clear();
+})
+
+
+//saving into local storage on search button
+var searchBtn = document.getElementById("searchLocBtn");
+searchBtn.addEventListener("click", function() {
+    var locationInput = document.getElementById("locationSearch");
+    var cityElement = locationInput;
+    saveCity(cityElement); 
+    console.log("poop");
+})
+
+
 
 
 
 //function to display current weather and also 5 day forecast below
 
 
+
+
+
+
+
+
 //saving in local storage
 var saveCity = function(cityElement) {
+    console.log("poop");
 
     if (localStorage.getItem("cityData") == null) {
         var newArray = []
@@ -18,9 +41,18 @@ var saveCity = function(cityElement) {
     } else {
         //array already exists in storage
         var currentCityData = JSON.parse(localStorage.getItem("cityData"))
-        
-        for (i = 0; i < currentCityData.length; i++) {
-            if (currentCityData[i].)
+        var cityExists = false;
+
+        for( i = 0; i < currentCityData.length; i++) {
+            if (currentCityData[i] == cityElement) {
+                cityExists = true;
+            }
+        }
+        if (cityName) {
+            currentCityData.push(cityElement);
+            localStorage.setItem("cityData", JSON.stringify(currentCityData));
         }
     }
-}
+};
+
+
