d53dd0a1b3e9a761b7122de6cbea205e7dcf16a9
lverghese <loganverghese@gmail.com>
2021-07-27 22:14:05 -0400

    local storage saving start

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,18 @@ const APIKey = "40360ebbfafb9db4e0cd8e687316fac5";
 
 
 //saving in local storage
+var saveCity = function(cityElement) {
+
+    if (localStorage.getItem("cityData") == null) {
+        var newArray = []
+        newArray.push(cityElement)
+        localStorage.setItem("cityData", JSON.stringify(newArrary));
+    } else {
+        //array already exists in storage
+        var currentCityData = JSON.parse(localStorage.getItem("cityData"))
+        
+        for (i = 0; i < currentCityData.length; i++) {
+            if (currentCityData[i].)
+        }
+    }
+}
